22a4530ae020e7f8add216b43ef3ebb4896957ff
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-12-18 14:23:28 +0200

    Add comments to ItemCreator.

--- src/ItemCreator.js
+++ src/ItemCreator.js
@@ -1,7 +1,9 @@
 import React from "react";
 import HasOnlyWhiteSpace from "./HasOnlyWhiteSpace";
 
+// A form with a text input and a submit button that sends input text to parent component
 class ItemCreator extends React.Component {
+    // Variable that stores the current string in the text input
     inputText = "";
     render() {
         return (
@@ -11,7 +13,9 @@ class ItemCreator extends React.Component {
                         event.preventDefault();
                         // Don't do anything if the input only has white space
                         if (!HasOnlyWhiteSpace(this.inputText)) {
+                            // Remove text from the text input
                             document.getElementById(this.props.id).value = "";
+                            // Send input text to parent and reset it
                             this.props.onSubmit(this.inputText);
                             this.inputText = "";
                         }
@@ -21,8 +25,9 @@ class ItemCreator extends React.Component {
                         className="TextInput"
                         placeholder={this.props.placeholder}
                         autoComplete="off"
-                        onChange={(event) => (this.inputText = event.target.value)}
                         id={this.props.id}
+                        // Updates variable to the current text in the text input
+                        onChange={(event) => (this.inputText = event.target.value)}
                     />
                     <input type="submit" value="Add" />
                 </form>
