3b46b4a4ea13c4c9149273c17ab4c8c22009f191
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2021-12-18 13:18:51 +0200

    Add comment to TasksPage.

--- src/TasksPage.js
+++ src/TasksPage.js
@@ -5,6 +5,7 @@ import Tag from "./Tag";
 import axios from "axios";
 import ItemCreator from "./ItemCreator";
 
+// The "Manage tasks" page
 class TasksPage extends React.Component {
     constructor(props) {
         super(props);
